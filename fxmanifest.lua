20c643a90889f05be328726eebe9eb3f80086c99
xinput <146970580+xinputdevelopment@users.noreply.github.com>
2025-08-14 19:00:32 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,9 +3,10 @@ game 'gta5'
 lua54 'yes'
 
 author 'AbdieZ'
-description '/samtal kommando för staff medlemmar'
+description '/samtal command for staff members.'
 version '1.0.0'
 
 client_scripts { 'src/client/*.lua' }
 server_scripts { 'src/server/*.lua' }
 shared_scripts { 'configuration/*.lua' }
+
